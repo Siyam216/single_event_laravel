b8575cc620274e1d579e424396ce831ab7ac4612
Siyam216 <khansiyam216@gmail.com>
2025-10-16 16:37:09 +0600

    all models

--- app/Models/Accommodation.php
+++ app/Models/Accommodation.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//all models
 class Accommodation extends Model
 {
     use HasFactory;
