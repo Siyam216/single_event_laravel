bcd6bec036fdaf9cdf147150c1e7798eb2ba8f1c
Siyam216 <khansiyam216@gmail.com>
2025-10-22 20:20:09 +0600

    speker blade has complited

--- resources/views/admin/speaker_schedule/index.blade.php
+++ resources/views/admin/speaker_schedule/index.blade.php
@@ -7,6 +7,7 @@
         <div class="section-header">
             <h1>Assign Schedule to Speaker</h1>
         </div>
+       
         <div class="section-body">
             <div class="row">
                 <div class="col-12">
